59c6833aec9989cb14a6a31bd53470c335a47d13
Gjorgjirutev <gjorgjirutev@gmail.com>
2023-12-06 23:26:53 +0100

    prva verzija od kodot na gjorgji

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from openai import OpenAI
 from docx import Document
 client = OpenAI()
-audio_file_path = open("audio.mp3", "rb")
-peder =
+audio_file_path = open("audio.mp3", "rb") w
+text = audio_file_path
 def transcribe_audio(audio_file_path):
     with open(audio_file_path, 'rb') as audio_file:
         transcription = client.audio.transcriptions.create("whisper-1", audio_file)
-    return transcription[peder]
+    return transcription[text]
 
-print(peder)
+print(text)
